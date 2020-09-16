ed436a2e280f164724e81e1ed9737cb4ad957440
Roman <65686226+morozit@users.noreply.github.com>
2020-09-16 15:47:36 +0300

    Update task-6-10.js

--- js/task/task-6-10.js
+++ js/task/task-6-10.js
@@ -1,4 +1,4 @@
-console.log("Задача 6-5");
+console.log("Задача 6-10");
 
 const users = [
   {
@@ -87,35 +87,26 @@ const users = [
   },
 ];
 
-const getUserWithEmail = (array, mail) =>
-  array.find(({ email }) => email === mail);
+const getSortedUniqueSkills = (array) =>
+  array
+    .reduce((acc, { skills }) => [...acc, ...skills], [])
+    .filter((el, index, array) => array.indexOf(el) === index)
+    .sort();
 
-console.log(getUserWithEmail(users, "rossvazquez@xinware.com"));
+// const getSortedUniqueSkills = (array) =>
+//   array
+//     .reduce((acc, { skills }) => [...acc, ...skills], [])
+//     .filter((el, a.localCompare(b)));
 
-/* {
-    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-    name: 'Ross Vazquez',
-    email: 'rossvazquez@xinware.com',
-    eyeColor: 'green',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-    isActive: false,
-    balance: 3793,
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-    gender: 'male',
-    age: 24,
-} */
+console.log(getSortedUniqueSkills(users));
 
-console.log(getUserWithEmail(users, "blackburndotson@furnigeer.com"));
-
-/* {
-    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
-    name: 'Blackburn Dotson',
-    email: 'blackburndotson@furnigeer.com',
-    eyeColor: 'brown',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-    isActive: false,
-    balance: 1498,
-    skills: ['non', 'amet', 'ipsum'],
-    gender: 'male',
-    age: 38,
-} */
+/* [ 'adipisicing', 'amet',
+ 'anim', 'commodo',
+ 'culpa', 'elit',
+ 'ex', 'ipsum',
+ 'irure', 'laborum',
+ 'lorem', 'mollit',
+ 'non', 'nostrud',
+ 'nulla', 'proident',
+ 'tempor', 'velit',
+ 'veniam' ]; */
